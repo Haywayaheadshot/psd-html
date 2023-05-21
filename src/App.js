8324e92a28490cc39be83bccad8aaa80bd8e7d70
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-21 17:51:48 +0000

    install react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import HistoryPage from './components/HistoryPage/HistoryPage';
 import LandingPage from './components/LandingPage/LandingPage';
 import NavBar from './components/NavBar/NavBar';
 
 const App = () => (
   <div>
-    <NavBar />
-    <LandingPage />
+    <Router>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/history" element={<HistoryPage />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
   </div>
 );
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
