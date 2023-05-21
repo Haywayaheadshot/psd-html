3b714f4f1dd201bfc234989d6a7fe01d96f6604e
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-21 17:51:35 +0000

    create Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 const NavBar = () => (
-  <nav className="nav-container">
-    <img src={logo} alt="Los Angeles Mountains Logo" className="page-logo" />
-    <ul>
-      <li>01. History</li>
-      <li>03. Team</li>
+  <nav className="flex flex-row justify-between items-center w-full px-5 py-2 fixed">
+    <NavLink to="/">
+      <img src={logo} alt="Los Angeles Mountains Logo" className="w-[40px]" />
+    </NavLink>
+    <ul className="flex flex-row gap-5 text-[12px] tracking-[1px] uppercase text-[#fefeff] font-black italic underline underline-offset-4">
+      <NavLink to="/history">
+        <li>01. History</li>
+      </NavLink>
+      <li>02. Team</li>
     </ul>
   </nav>
 );
